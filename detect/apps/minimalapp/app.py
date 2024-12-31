fe847566683f2051963a9d9e902e9f6cade78eeb
takehiro1111 <t.soccer.0916@gmail.com>
2024-12-31 23:51:39 +0900

    メール送信機能の実装

--- detect/apps/minimalapp/app.py
+++ detect/apps/minimalapp/app.py
@@ -1,5 +1,8 @@
 
 from email_validator import EmailNotValidError, validate_email
+import logging
+from flask_debugtoolbar import DebugToolbarExtension 
+import os
 from flask import (
   Flask,
   render_template,
@@ -9,6 +12,12 @@ from flask import (
   request,
   flash
 )
+from flask_mail import Mail,Message
+import my_gmail_account
+import smtplib, ssl
+from email.mime.text import MIMEText
+
+send_address = my_gmail_account.account
 
 # インスタンス化
 app = Flask(__name__)
@@ -18,6 +27,7 @@ app.config.update(
     DEBUG=True
 )
 
+# アプリケーションコンテキストの実行環境を有効化
 ctx=app.app_context()
 ctx.push()
 
@@ -30,8 +40,28 @@ print(g.connection)
 # with app.test_request_context():  
 #   print(url_for("hello",name="takehiro1111"))
 
+# test_request_context -> 擬似リクエストしてくれるメソッド
 with app.test_request_context("/users?updated=true"):
   print(request.args.get("updated"))
+  
+# ロギング
+app.logger.setLevel(logging.DEBUG)
+app.logger.debug("Debug Logger")
+
+# デバッグツール
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+toolbar = DebugToolbarExtension(app)
+
+# Mailクラスのconfigを追加
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_POST"] = os.environ.get("MAIL_POST")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+# flask-mail拡張の登録
+mail = Mail(app)
 
 ################################################
 # Hello World
@@ -76,7 +106,7 @@ def contact_complete():
       flash('ユーザー名は必須です。')
       is_valid = False
       
-    if not email:
+    elif not email:
       flash('メールアドレスは必須です。')
       is_valid = False
       
@@ -89,12 +119,19 @@ def contact_complete():
     if not description:
       flash(f"問い合わせ内容は必須です。")
       is_valid = False
-      
-    if not is_valid:
+  
+    elif not is_valid:
       return redirect(url_for("contact"))
   
-  # ↓メールを送信する処理の実装
-    
+  # メールを送信する処理の実装
+    # send_email(
+    #   email,
+    #   "お問い合わせありがとうございます。",
+    #   "contact_mail",
+    #   username=username,
+    #   description=description
+    # )
+    send_test_email()
 
     flash("問い合わせ内容はメールにて送信しました。問い合わせありがとうございます。")
     return redirect(url_for("contact_complete"))
@@ -104,7 +141,41 @@ def contact_complete():
 def internal_server_error(e):
     app.logger.error(f'Server Error: {e}')
     return 'Internal Server Error from Logger', 500
+  
+# def send_email(to,subject,template,**kwargs):
+#   """
+#   for mail send function
+#   """
+#   msg = Message(subject,recipients =[to])
+#   msg.body = render_template(f"{template}.txt",**kwargs)
+#   msg.html = render_template(f"{template}.html",**kwargs)
 
+  
+# メインの関数になります
+def send_test_email():
+  msg = make_mime_text(
+    mail_to = send_address,
+    subject = "テスト送信",
+    body = "Pythonでのメール送信です"
+  )
+  send_gmail(msg)
+
+# 件名、送信先アドレス、本文を渡す関数です
+def make_mime_text(mail_to, subject, body):
+  msg = MIMEText(body, "html")
+  msg["Subject"] = subject
+  msg["To"] = mail_to
+  msg["From"] = send_address
+  return msg
+
+# smtp経由でメール送信する関数です
+def send_gmail(msg):
+  server = smtplib.SMTP_SSL(
+    "smtp.gmail.com", 465,
+    context = ssl.create_default_context())
+  server.set_debuglevel(0)
+  server.login(send_address, my_gmail_account.password)
+  server.send_message(msg)
 
 if __name__ == "__main__":
     app.run(debug=True)
