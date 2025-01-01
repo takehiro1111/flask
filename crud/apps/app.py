0e9500d89632f01439ad336ece8dd4306ef3c070
takehiro1111 <t.soccer.0916@gmail.com>
2025-01-01 23:14:11 +0900

    blueprintを用いたページ表示

--- crud/apps/app.py
+++ crud/apps/app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
+import views,app
 
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#   return render_template("crud/index.html")
+  
 def create_app():
-  app = Flask(__name__)
-  
-  import views
-  
   # register_blueprint関数を用いて、viewsのcrudをアプリへ登録する。
   app.register_blueprint(views.crud,url_prefix="/crud")
   return app
 
+# flask runで実行されるようにグローバルで設定。
+create_app()
+
 if __name__ == "__main__":
-    import app
+    app = create_app()
     app.run(debug=True)
